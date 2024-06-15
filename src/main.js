65e192869a67777999775e6baf7ba0116fd05ca9
Mura41 <muramase26@gmail.com>
2024-06-15 17:28:40 +1200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import App from './App.vue'
 
 import Home from './pages/Home.vue'
 import Favorites from './pages/Favorite.vue'
-import Product from './pages/Product.vue'
+
 import Register from './components/User.vue'
 import EditUser from './components/EditUser.vue'
 
@@ -17,7 +17,7 @@ const app = createApp(App)
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/favorites', name: 'Favorites', component: Favorites },
-  { path: '/product/:id', name: 'Product', component: Product },
+
   { path: '/register', name: 'Register', component: Register },
   { path: '/edit-profile', name: 'EditUser', component: EditUser }
 ]
