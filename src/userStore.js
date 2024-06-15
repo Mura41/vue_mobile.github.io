3669795f8f6d87d7daa2d5c100acaf0bbb5dac50
Mura41 <muramase26@gmail.com>
2024-06-15 17:29:15 +1200

    Update userStore.js

--- src/userStore.js
+++ src/userStore.js
@@ -3,14 +3,21 @@ import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
-    username: null
+    userName: null,
+    email: null
   }),
   actions: {
-    login(name) {
-      this.username = name
+    login(name, email) {
+      this.userName = name
+      this.email = email
     },
     logout() {
-      this.username = null
+      this.userName = null
+      this.email = null
+    },
+    updateUser({ userName, email }) {
+      this.userName = userName
+      this.email = email
     }
   }
 })
