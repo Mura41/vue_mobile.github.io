d2c3ac570737868ae6b279dfc9f05a7c48ddb5e5
Mura41 <muramase26@gmail.com>
2024-06-15 17:31:06 +1200

    Update CardList.vue

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,7 +1,8 @@
 <script setup>
+import { computed } from 'vue'
 import Card from './Card.vue'
 
-defineProps({
+const props = defineProps({
   items: Array,
   isFavorites: Boolean
 })
@@ -19,7 +20,32 @@ const handleAddToFavorite = (item) => {
     emit('addToFavorite', item)
   }
 }
+
+// Фильтрация продуктов, у которых нет цены или изображения
+const filteredItems = computed(() => {
+  return props.items.filter((item) => item.price && item.imageUrl)
+})
+
+console.log('Original items:', props.items)
+console.log('Filtered items:', filteredItems.value)
 </script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5" v-auto-animate>
+    <Card
+      v-for="item in filteredItems"
+      :key="item.id"
+      :id="item.id"
+      :title="item.title"
+      :imageUrl="item.imageUrl"
+      :price="item.price"
+      :isFavorite="item.isFavorite"
+      :isAdded="item.isAdded"
+      @onClickAdd="handleAddToCart(item)"
+      @onClickFavorite="handleAddToFavorite(item)"
+    />
+  </div>
+</template>
 
 <style scoped>
 .card {
@@ -36,20 +62,3 @@ const handleAddToFavorite = (item) => {
   font-weight: 400; /* Regular */
 }
 </style>
-
-<template>
-  <div class="grid grid-cols-4 gap-5" v-auto-animate>
-    <Card
-      v-for="item in items"
-      :key="item.id"
-      :id="item.id"
-      :title="item.title"
-      :imageUrl="item.imageUrl"
-      :price="item.price"
-      :isFavorite="item.isFavorite"
-      :isAdded="item.isAdded"
-      @onClickAdd="handleAddToCart(item)"
-      @onClickFavorite="handleAddToFavorite(item)"
-    />
-  </div>
-</template>
