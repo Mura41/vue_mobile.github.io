f39dcc1c25886ba85c5cd25a56cdeff887bc5b00
Mura41 <muramase26@gmail.com>
2024-06-15 18:23:00 +1200

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -126,18 +126,18 @@ watch(filters, fetchItems)
   <div class="flex justify-between items-center">
     <h2 class="text-3xl font-bold mb-8">Все товары</h2>
 
-    <div class="flex gap-4 items-center">
-      <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none text-sm w-40">
+    <div class="flex gap-4">
+      <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
         <option value="name">По названию</option>
         <option value="price">По цене (дешевые)</option>
         <option value="-price">По цене (дорогие)</option>
       </select>
 
       <div class="relative">
-        <img class="absolute left-4 top-3 w-4" src="/search.svg" />
+        <img class="absolute left-4 top-3" src="/search.svg" />
         <input
           @input="onChangeSearchInput"
-          class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400 text-sm w-40"
+          class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
           type="text"
           placeholder="Поиск..."
         />
@@ -155,8 +155,3 @@ watch(filters, fetchItems)
     />
   </div>
 </template>
-
-<style scoped>
-/* Добавьте свои стили здесь */
-</style>
-
